b2c71acf1b2b3e019df98eaf621b6d3fcd494afa
Frederico Meira <fredericomeiraa@gmail.com>
2021-10-15 02:13:25 +0100

    shipping to 42

--- src/prints.c
+++ src/prints.c
@@ -5,10 +5,12 @@ int	print_msg(char *msg, t_phil *phil)
 	long long	starve;
 
 	starve = phil->last_eated - phil->env->start_time;
-	if (!phil->env->no_deads || starve > phil->env->die_time)
+	if (!phil->env->no_deads)
+		return (1);
+	if (starve > phil->env->die_time)
 	{
 		phil->env->no_deads = false;
-		printf("%lli\t%d %s\n",curr_time(), phil->nbr, "died");
+		printf("%lli\t%d %s\n", (curr_time() - phil->env->start_time), phil->nbr, "died");
 		return (1);
 	}
 	printf("%lli\t%d %s\n", (curr_time() - phil->env->start_time), phil->nbr, msg);
