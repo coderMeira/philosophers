3ed886e48face401b18b8b04214245cfc161b5dc
Frederico Meira <fmeira@c1r17s11.42lisboa.com>
2021-10-22 20:13:43 +0100

    program seems to be working as expected. needs more testing and norm

--- src/time.c
+++ src/time.c
@@ -6,7 +6,7 @@
 /*   By: fmeira <fmeira@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 19:53:12 by fmeira            #+#    #+#             */
-/*   Updated: 2021/10/21 13:59:47 by fmeira           ###   ########.fr       */
+/*   Updated: 2021/10/22 19:44:07 by fmeira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	ft_usleep(long long start_time, long long sleeping_time, t_phil *phil)
 {
 	while ((curr_time() - start_time) <= sleeping_time)
 	{
+		if (!phil->env->no_deads)
+			return (1);
 		if ((curr_time() - phil->last_eated) >= phil->env->die_time)
 			return (kill(phil));
 		//usleep(sleeping_time - sleeping_time / 1);
